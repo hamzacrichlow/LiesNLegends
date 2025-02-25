906bedb0a913682d3175f274bb34bd1b901d06aa
Derald Blesman <dblessman24@gmail.com>
2025-02-25 10:36:22 -0500

    Initial Commit

---
+++ LiesNLegendsOfficial/LiesNLegendsOfficialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiesNLegendsOfficialApp.swift
+//  LiesNLegendsOfficial
+//
+//  Created by Derald Blessman on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct LiesNLegendsOfficialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiesNLegendsOfficialTests/LiesNLegendsOfficialTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiesNLegendsOfficialTests.swift
+//  LiesNLegendsOfficialTests
+//
+//  Created by Derald Blessman on 2/25/25.
+//
+
+import Testing
+@testable import LiesNLegendsOfficial
+
+struct LiesNLegendsOfficialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
